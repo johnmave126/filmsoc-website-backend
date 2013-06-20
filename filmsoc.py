f5a5d4477710b0171d38a8ef1915cd0f18dccbbb
JohnMave126 <johnmave126@gmail.com>
2013-06-20 12:40:46 +0800

    add profiler to help Signed-off-by: JohnMave126 <johnmave126@gmail.com>

--- filmsoc.py
+++ filmsoc.py
@@ -1,3 +1,4 @@
+from repoze.profile.profiler import AccumulatingProfileMiddleware
 from app import app, db
 
 from auth import *
@@ -9,4 +10,14 @@ from api import api
 api.setup()
 
 if __name__ == '__main__':
+    wsgi_app = app.wsgi_app
+    wrapped = AccumulatingProfileMiddleware(
+        wsgi_app,
+        log_filename='wsgi.prof',
+        discard_first_request=True,
+        flush_at_shutdown=True,
+        path='/__profile__'
+        )
+    app.wsgi_app = wrapped
+    #serve(app)
     app.run(host='0.0.0.0', port=5950, debug=True)
