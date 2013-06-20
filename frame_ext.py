daadb9f0e48d216c58837da185ae4d60fdb4d6ff
JohnMave126 <johnmave126@gmail.com>
2013-06-20 11:41:00 +0800

    remove debug. fix ignore case Signed-off-by: JohnMave126 <johnmave126@gmail.com>

--- frame_ext.py
+++ frame_ext.py
@@ -324,8 +324,7 @@ class CustomResource(RestResource):
         return self.response({'deleted': res})
 
     def apply_search_query(self, query, terms, fields):
-        query_clauses = [reduce(operator.or_, [DQ(**{"%s__like" % y: "%%%s%%" % x}) for y in fields]) for x in terms]
-        raise ValueError(0)
+        query_clauses = [reduce(operator.or_, [DQ(**{"%s__ilike" % y: "%%%s%%" % x}) for y in fields]) for x in terms]
         return query.filter(reduce(operator.and_, query_clauses))
 
     def api_search(self):
