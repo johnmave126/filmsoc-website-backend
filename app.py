976f2f29f2f26e37bd78e09e190cc3d34180d03c
JohnMave126 <johnmave126@gmail.com>
2013-07-05 22:22:01 +0800

    ... Signed-off-by: JohnMave126 <johnmave126@gmail.com>

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ class cFlask(Flask):
         rv = super(cFlask, self).make_default_options_response()
         h = rv.headers
         h['Access-Control-Allow-Origin'] = 'http://ihome.ust.hk'
-        h['Access-Control-Allow-Methods'] = str(rv.allow)
-        self.logger.notice(str(rv.allow))
+        #h['Access-Control-Allow-Methods'] = str(rv.allow)
+        self.logger.notice(rv.allow)
         return rv
 
 #app and database
