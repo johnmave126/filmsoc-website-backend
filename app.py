71e7659bb60f87618f18114a50084836fa88c1e7
JohnMave126 <johnmave126@gmail.com>
2013-07-06 12:44:02 +0800

    Control headers on apache server level Signed-off-by: JohnMave126 <johnmave126@gmail.com>

--- app.py
+++ app.py
@@ -12,15 +12,6 @@ app.config.from_object('settings.Settings')
 db = Database(app)
 
 
-@app.after_request
-def cros_headers(response):
-    h = response.headers
-    h['Access-Control-Allow-Origin'] = 'http://ihome.ust.hk'
-    h['Access-Control-Allow-Methods'] = h['Allow']
-    h['Access-Control-Allow-Headers'] = 'origin, content-type, accept, x-requested-with'
-    return response
-
-
 @app.route('/')
 def index():
     return redirect(app.config['FRONT_SERVER'])
