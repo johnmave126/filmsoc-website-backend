28559c63bb87c2916457a59e2c462c123f1d6f19
JohnMave126 <johnmave126@gmail.com>
2013-07-14 23:04:36 +0800

    not my fault. wait for peewee fix Signed-off-by: JohnMave126 <johnmave126@gmail.com>

--- frame_ext.py
+++ frame_ext.py
@@ -372,9 +372,6 @@ class CustomResource(RestResource):
 
     def apply_search_query(self, query, terms, fields):
         query_clauses = [reduce(operator.or_, [DQ(**{"%s__ilike" % y: z}) for y in fields]) for z in ("%%%s%%" % x for x in terms)]
-        for x in terms:
-            for y in fields:
-                app.logger.warning(query.filter(DQ(**{"%s__ilike" % y: "%%%s%%" % x})).sql())
         return query.filter(reduce(operator.and_, query_clauses))
 
     def api_list(self):
