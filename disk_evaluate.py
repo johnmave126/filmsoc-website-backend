63a338b4c6c67d78b326c6c1a5e7ec50a8a16db8
JohnMave126 <johnmave126@gmail.com>
2013-07-29 18:20:50 +0800

    Fix typo Signed-off-by: JohnMave126 <johnmave126@gmail.com>

--- disk_evaluate.py
+++ disk_evaluate.py
@@ -6,7 +6,7 @@ def main():
     disk_sq = Disk.select()
     for disk in disk_sq:
         ups, downs = disk.get_rate()
-        disk.confidence = confidence(ups, downs)
+        disk.rank = confidence(ups, downs)
         disk.save()
 
 
