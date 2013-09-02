bb2fa19c13fe93be75e4e2892652a319d4f31e09
JohnMave126 <johnmave126@gmail.com>
2013-09-02 11:14:14 +0800

    Operate on mailing list Signed-off-by: JohnMave126 <johnmave126@gmail.com>

--- expire.py
+++ expire.py
@@ -8,12 +8,12 @@ def main():
     	User.member_type != 'Expired',
     	User.expire_at < date.today()
     )
-    print join([(x.itsc + "@ust.hk") for x in user_sq], ';')
-    #for user in user_sq:
-    #    user.member_type = 'Expired'
-    #    user.save()
+    #print join([(x.itsc + "@ust.hk") for x in user_sq], ';')
+    for user in user_sq:
+        user.member_type = 'Expired'
+        user.save()
 
-    #update_mailing_list([x.itsc for x in User.select(User.itsc).where(User.member_type != 'Expired')])
+    update_mailing_list([x.itsc for x in User.select(User.itsc).where(User.member_type != 'Expired')])
 
 if __name__ == '__main__':
     main()
