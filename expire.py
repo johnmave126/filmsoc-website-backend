dd7f03ebbdf6015ac308e3f34546511df297c772
JohnMave126 <johnmave126@gmail.com>
2013-09-02 11:09:29 +0800

    Fix typo Signed-off-by: JohnMave126 <johnmave126@gmail.com>

--- expire.py
+++ expire.py
@@ -1,14 +1,14 @@
 from datetime import date
 from models import User
 from helpers import update_mailing_list
-
+from string import join
 
 def main():
     user_sq = User.select().where(
     	User.member_type != 'Expired',
     	User.expire_at < date.today()
     )
-    print [(x.itsc + "@ust.hk") for x in user_sq].join(';')
+    print join([(x.itsc + "@ust.hk") for x in user_sq], ';')
     #for user in user_sq:
     #    user.member_type = 'Expired'
     #    user.save()
