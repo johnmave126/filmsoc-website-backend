c8740883eab8eb9ac00b65d4e501d0d260808715
JohnMave126 <johnmave126@gmail.com>
2013-09-11 15:45:35 +0800

    Fix typo Signed-off-by: JohnMave126 <johnmave126@gmail.com>

--- reminder.py
+++ reminder.py
@@ -11,13 +11,13 @@ def main():
         Disk.due_at == date.today() + timedelta(1)
     )
     for disk in neardue:
-        last_log = Log.select().where(Log.model == 'Disk', Log.model_refer == disk.id, Log.Type == 'borrow', Log.user_affected == disk.borrowed_by).order_by(Log.created_at.desc()).get()
+        last_log = Log.select().where(Log.model == 'Disk', Log.model_refer == disk.id, Log.Type == 'borrow', Log.user_affected == disk.hold_by).order_by(Log.created_at.desc()).get()
         if 'renew' not in last_log.content:
             body = render_template('reminder.html', disk=disk)
-            send_email([disk.borrowed_by.itsc + '@ust.hk'], ['su_film@ust.hk'], 'Reminder: Due Date of the VCD/DVD(s) You Borrowed', body)
+            send_email([disk.hold_by.itsc + '@ust.hk'], ['su_film@ust.hk'], 'Reminder: Due Date of the VCD/DVD(s) You Borrowed', body)
         else:
             body = render_template('renewed_reminder.html', disk=disk)
-            send_email([disk.borrowed_by.itsc + '@ust.hk'], ['su_film@ust.hk'], 'Reminder: Due Date of the VCD/DVD(s) You Renewed', body)
+            send_email([disk.hold_by.itsc + '@ust.hk'], ['su_film@ust.hk'], 'Reminder: Due Date of the VCD/DVD(s) You Renewed', body)
     overdue = Disk.select().where(
         Disk.avail_type == 'Borrowed',
         Disk.due_at < date.today()
@@ -26,10 +26,10 @@ def main():
         passed = date.today() - Disk.due_at
         if passed.days % 3 == 1:
             body = render_template('overdue.html', disk=disk)
-            send_email([disk.borrowed_by.itsc + '@ust.hk'], ['su_film@ust.hk'], 'Reminder: Overdue of the VCD/DVD(s)', body)
+            send_email([disk.hold_by.itsc + '@ust.hk'], ['su_film@ust.hk'], 'Reminder: Overdue of the VCD/DVD(s)', body)
     reserved = Disk.select().where(Disk.avail_type == 'Reserved')
     for disk in reserved:
-        reserve_log = Log.select().where(Log.model == 'Disk', Log.model_refer == disk.id, Log.Type == 'reserve', Log.user_affected == disk.borrowed_by).order_by(Log.created_at.desc()).get()
+        reserve_log = Log.select().where(Log.model == 'Disk', Log.model_refer == disk.id, Log.Type == 'reserve', Log.user_affected == disk.hold_by).order_by(Log.created_at.desc()).get()
         if date.today() - reserve_log.created_at.date() > timedelta(2):
             disk.reserved_by = None
             disk.avail_type = 'Available'
