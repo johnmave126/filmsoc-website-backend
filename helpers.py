3969ad6bf94b43b964d5d38233709947dda3d27e
johnmave126 <johnmave126@gmail.com>
2014-01-11 18:58:10 +0800

    Refine code. Add comment and follow PEP8 Signed-off-by: JohnMave126 <johnmave126@gmail.com>

--- helpers.py
+++ helpers.py
@@ -1,20 +1,14 @@
 from math import sqrt
-from flask import g
-from app import app
-
+from ftplib import FTP
+import smtplib
 import ldap
 from ldap.filter import escape_filter_chars
-import urllib2
-from urllib import urlencode
-import sympa
-import re
-from string import join
-from ftplib import FTP
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.utils import COMMASPACE, formatdate
-import smtplib
-
+from flask import g
+import sympa
+from app import app
 
 __all__ = [
     'after_this_request',
@@ -27,6 +21,8 @@ __all__ = [
 
 
 def after_this_request(f):
+    """A function wrapper to register action after this Flask request"""
+
     if not hasattr(g, 'after_request_callbacks'):
         g.after_request_callbacks = []
     g.after_request_callbacks.append(f)
@@ -35,13 +31,23 @@ def after_this_request(f):
 
 @app.after_request
 def call_after_request_callbacks(response):
+    """Register to Flask after_request for after_this_request"""
+
     for callback in getattr(g, 'after_request_callbacks', ()):
         response = callback(response)
     return response
 
 
 def confidence(ups, downs):
-    # Wilson score confidence interval for a Bernoulli parameter
+    """Return Wilson score confidence interval for a Bernoulli parameter
+
+    ups -- The +1s
+    downs -- The -1s
+
+    Note that the calculation is modified so that there is weight to -1s
+    The return value is a float number with sign
+    """
+
     n = ups + downs
 
     if n == 0:
@@ -49,22 +55,33 @@ def confidence(ups, downs):
 
     z = 1.0
     phat = float(ups) / n
-    phat_volume = (phat + (z * z) / (2 * n) - z * sqrt((phat * (1 - phat) + z * z / (4 * n)) / n)) / (1 + z * z / n)
+    phat_volume = (phat + (z * z) / (2 * n) -
+                    z * sqrt((phat * (1 - phat) + z * z / (4 * n)) / n)) / \
+                    (1 + z * z / n)
     quat = float(downs) / n
-    quat_volume = (quat + (z * z) / (2 * n) - z * sqrt((quat * (1 - quat) + z * z / (4 * n)) / n)) / (1 + z * z / n)
+    quat_volume = (quat + (z * z) / (2 * n) -
+                    z * sqrt((quat * (1 - quat) + z * z / (4 * n)) / n)) / \
+                    (1 + z * z / n)
     return phat_volume - quat_volume / 2
 
 
-def query_user(itsc):
+def query_user(itsc, attrs = ['displayName']):
+    """Connect to HKUST Active Directory to query relative infomation
+
+    itsc -- The ITSC account of the person queried
+    attrs -- The information to query (default ['displayName'])
+
+    Return a dict containing the information
+    """
     conn = ldap.initialize(app.config['LDAP_SERVER'])
     conn.set_option(ldap.OPT_REFERRALS, 0)
     conn.set_option(ldap.OPT_PROTOCOL_VERSION, 3)
     try:
-        conn.simple_bind_s('HKUST\\' + app.config['SOCIETY_USERNAME'], app.config['SOCIETY_PASSWORD'])
+        conn.simple_bind_s('HKUST\\' + app.config['SOCIETY_USERNAME'],
+                            app.config['SOCIETY_PASSWORD'])
 
         base_dn = 'CN=Users,DC=ust,DC=hk'
         Filter = '(CN=' + escape_filter_chars(itsc) + ')'
-        attrs = ['displayName', 'whenCreated']
 
         r = conn.search(base_dn, ldap.SCOPE_ONELEVEL, Filter, attrs)
         Type, data = conn.result(r, timeout=10)
@@ -85,6 +102,10 @@ def query_user(itsc):
 
 
 def update_mailing_list(new_list):
+    """Update the mailing list
+
+    new_list -- The list to replace the present
+    """
     sympa_mgmt = sympa.Sympa(
         app.config['SOCIETY_USERNAME'],
         app.config['SOCIETY_PASSWORD'],
@@ -96,17 +117,31 @@ def update_mailing_list(new_list):
 
 
 def upload_file(filename, file_handler):
-    conn = FTP('ihome.ust.hk', app.config['SOCIETY_USERNAME'], app.config['SOCIETY_PASSWORD'])
+    """Upload a file to FTP server
+
+    filename -- The name to be stored in FTP
+    file_handler -- The file to be uploaded
+    """
+    conn = FTP('ihome.ust.hk', app.config['SOCIETY_USERNAME'],
+                app.config['SOCIETY_PASSWORD'])
     conn.cwd('/asset/upload')
     conn.storbinary("STOR " + filename, file_handler)
     conn.quit()
 
 
 def send_email(receiver, bcc, subject, body):
+    """Send an email
+
+    receiver -- A list of receivers
+    bcc -- A list of receivers who will not be shown in to:
+    subject -- The subject of the email
+    body -- The body of the email
+    """
     from_address = app.config['SOCIETY_USERNAME'] + '@ust.hk'
 
     msg = MIMEMultipart()
-    msg['From'] = msg['Reply-To'] = '"Film Society, HKUSTSU"<%s>' % from_address
+    msg['From'] = msg['Reply-To'] = \
+        '"Film Society, HKUSTSU"<%s>' % from_address
     msg['To'] = COMMASPACE.join(receiver)
     msg['Subject'] = '[Film Society]' + subject
     msg['Date'] = formatdate(localtime=True)
