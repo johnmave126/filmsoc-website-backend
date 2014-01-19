2bee1ac1cce31a1db361005f904c8211e4a3f872
johnmave126 <johnmave126@gmail.com>
2014-01-19 18:34:55 +0800

    Fix regular expression Signed-off-by: JohnMave126 <johnmave126@gmail.com>

--- bbcode.py
+++ bbcode.py
@@ -15,11 +15,11 @@ class BBCode(object):
     def __init__(self):
         # predefined BB Code set
         self.BBHandler = [
-            {"pattern": r"\[b\](.+?)\[/b\]", "repl": '<b>\1</b>'},
-            {"pattern": r"\[i\](.+?)\[/i\]", "repl": '<em>\1</em>'},
-            {"pattern": r"\[u\](.+?)\[/u\]", "repl": '<u>\1</u>'},
-            {"pattern": r"\[big\](.+?)\[/big\]", "repl": '<big>\1</big>'},
-            {"pattern": r"\[small\](.+?)\[/small\]", "repl": '<small>\1</small>'},
+            {"pattern": r"\[b\](.+?)\[/b\]", "repl": r'<b>\1</b>'},
+            {"pattern": r"\[i\](.+?)\[/i\]", "repl": r'<em>\1</em>'},
+            {"pattern": r"\[u\](.+?)\[/u\]", "repl": r'<u>\1</u>'},
+            {"pattern": r"\[big\](.+?)\[/big\]", "repl": r'<big>\1</big>'},
+            {"pattern": r"\[small\](.+?)\[/small\]", "repl": r'<small>\1</small>'},
             {"pattern": r"\[color=([a-zA-Z]*|\#?[0-9a-fA-F]{6})\](.+?)\[/color\]",
                 "repl": '<span style="color:\1">\2</span>'},
             {"pattern": (r"\[link=\s*((?:(?:ftp|https?)://)?"
@@ -27,13 +27,13 @@ class BBCode(object):
                             "[A-Z]{2,6}\.?|localhost|"
                             "\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})"
                             "(?::\d+)?(?:/?|[/?]\S+?))\s*\](.+?)\[/link\]"),
-                "repl": '<a href="\1" rel="nofollow" target="_blank">\2</a>'},
+                "repl": r'<a href="\1" rel="nofollow" target="_blank">\2</a>'},
             {"pattern": (r"\[img=\s*((?:(?:ftp|https?)://)?"
                             "(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+"
                             "[A-Z]{2,6}\.?|localhost|"
                             "\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})"
                             "(?::\d+)?(?:/?|[/?]\S+?))\s*\](.*?)\[/img\]"),
-                "repl": '<img src="\1" alt="\2" title="\2">'},
+                "repl": r'<img src="\1" alt="\2" title="\2">'},
             {"pattern": r"\[inlinedisk\](\d+)\[/inlinedisk\]",
                 "repl": self.parse_inlinedisk},
             {"pattern": r"\[disk\](\d+)\[/disk\]",
